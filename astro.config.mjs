761edb676d632755714952be1b11870c3a8f3ade
Ali Lee <fcqz121314@gmail.com>
2026-07-20 15:01:41 +0800

    chore: configure Astro for Cloudflare SSR

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import cloudflare from "@astrojs/cloudflare";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
